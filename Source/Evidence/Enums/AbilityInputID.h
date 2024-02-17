115e1249b2c8a0515fc6118ed283f32567386d36
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-02-17 11:28:24 +0000

    Create AbilityInputID.h

---
+++ Source/Evidence/Enums/AbilityInputID.h
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class EAbilityID : uint8
+{
+	Jump    UMETA(DisplayName = "Jump"),
+	MAX     UMETA(DisplayName = "MAX")
+};
