4f474918ddb854678785adc3eccb605913921607
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-02-17 14:49:45 +0000

    Added jump ability to character

---
+++ Source/Evidence/Character/Abilities/EIJumpAbility.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EIJumpAbility.h"
+#include "Evidence/Character/EvidenceCharacter.h"
+
+UEIJumpAbility::UEIJumpAbility()
+{
+	AbilityInputID = EAbilityInputID::Jump;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::NonInstanced;
+}
+
+void UEIJumpAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+	{
+		if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+		{
+			EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		}
+
+		ACharacter* Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
+		Character->Jump();
+	}
+}
+
+bool UEIJumpAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+	{
+		return false;
+	}
+
+	const AEvidenceCharacter* Character = CastChecked<AEvidenceCharacter>(ActorInfo->AvatarActor.Get(), ECastCheckedType::NullAllowed);
+	return Character && Character->CanJump();
+}
+
+void UEIJumpAbility::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	if (ActorInfo != NULL && ActorInfo->AvatarActor != NULL)
+	{
+		CancelAbility(Handle, ActorInfo, ActivationInfo, true);
+	}
+}
+
+void UEIJumpAbility::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
+{
+	if (ScopeLockCount > 0)
+	{
+		WaitingToExecute.Add(FPostLockDelegate::CreateUObject(this, &UEIJumpAbility::CancelAbility, Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility));
+		return;
+	}
+
+	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
+
+	ACharacter* Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
+	Character->StopJumping();
+}
