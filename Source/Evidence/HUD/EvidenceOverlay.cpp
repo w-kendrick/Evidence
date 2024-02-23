4262532efcf8861c34dd19d47fb2ae81acce798b
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-02-23 19:08:27 +0000

    Implemented functions to show and hide interact prompts

--- Source/Evidence/HUD/EvidenceOverlay.cpp
+++ Source/Evidence/HUD/EvidenceOverlay.cpp
@@ -2,13 +2,25 @@
 
 
 #include "EvidenceOverlay.h"
+#include "Components/CanvasPanel.h"
+#include "Components/WidgetSwitcher.h"
+#include "Components/CanvasPanel.h"
 
 void UEvidenceOverlay::ShowInteractPrompt(const float Duration)
 {
+	if (Duration > 0)
+	{
+		InteractSwitcher->SetActiveWidgetIndex(1);
+	}
+	else
+	{
+		InteractSwitcher->SetActiveWidgetIndex(0);
+	}
 
+	InteractCanvas->SetVisibility(ESlateVisibility::HitTestInvisible);
 }
 
 void UEvidenceOverlay::HideInteractPrompt()
 {
-
+	InteractCanvas->SetVisibility(ESlateVisibility::Hidden);
 }
--- Source/Evidence/HUD/EvidenceOverlay.h
+++ Source/Evidence/HUD/EvidenceOverlay.h
@@ -17,5 +17,15 @@ class EVIDENCE_API UEvidenceOverlay : public UUserWidget
 public:
 	void ShowInteractPrompt(const float Duration);
 	void HideInteractPrompt();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	class UCanvasPanel* InteractCanvas;
+
+	UPROPERTY(meta = (BindWidget))
+	class UWidgetSwitcher* InteractSwitcher;
+
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* InteractBar;
 	
 };
