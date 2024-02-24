04acef87e832842e7e588962429bc28a08507c5a
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-02-24 19:11:52 +0000

    Created equipment class

---
+++ Source/Evidence/Items/Equipment.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/AbilityActor.h"
+#include "Interactable.h"
+#include "Equipment.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AEquipment : public AAbilityActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+};
