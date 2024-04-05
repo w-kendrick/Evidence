45c88d4ccd18ebe9778779bc7e349d68656a4e47
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-04-05 23:24:26 +0100

    Created toggle power ability

---
+++ Source/Evidence/Items/Equipment/Abilities/TogglePowerAbility.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TogglePowerAbility.h"
+#include "Evidence/Items/Equipment/PoweredEquipment.h"
+
+UTogglePowerAbility::UTogglePowerAbility()
+{
+	AbilityInputID = EAbilityInputID::Use;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Use")));
+}
+
+void UTogglePowerAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		return;
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Black, "Toggle power");
+
+	APoweredEquipment* Powered = Cast<APoweredEquipment>(GetSourceObject(Handle, ActorInfo));
+
+	if (!Powered)
+	{
+		return;
+	}
+
+	Powered->ToggleActivation();
+}
