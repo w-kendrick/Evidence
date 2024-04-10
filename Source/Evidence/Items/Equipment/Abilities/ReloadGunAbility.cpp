0bef069b8ab866a0fbc8af649b115ab25ea62c39
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-04-10 21:06:13 +0100

    Added basic functions to reload ability

--- Source/Evidence/Items/Equipment/Abilities/ReloadGunAbility.cpp
+++ Source/Evidence/Items/Equipment/Abilities/ReloadGunAbility.cpp
@@ -2,11 +2,35 @@
 
 
 #include "ReloadGunAbility.h"
+#include "Evidence/Character/EvidenceCharacter.h"
+#include "Evidence/Character/Components/InventoryComponent.h"
 
 UReloadGunAbility::UReloadGunAbility()
 {
 	AbilityInputID = EAbilityInputID::Reload;
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
 	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
-	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Use")));
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Reload")));
+}
+
+void UReloadGunAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	AEvidenceCharacter* Char = Cast<AEvidenceCharacter>(GetCurrentActorInfo()->AvatarActor);
+	if (!Char)
+	{
+		return;
+	}
+
+	UInventoryComponent* InventoryComp = Char->GetInventoryComponent();
+	if (!InventoryComp)
+	{
+		return;
+	}
+
+	const TArray<uint8> AvailableAmmo = InventoryComp->FindAmmunitionOfType(AmmoClass);
+}
+
+bool UReloadGunAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	return false;
 }
--- Source/Evidence/Items/Equipment/Abilities/ReloadGunAbility.h
+++ Source/Evidence/Items/Equipment/Abilities/ReloadGunAbility.h
@@ -6,9 +6,8 @@
 #include "Evidence/Character/Abilities/EIGameplayAbility.h"
 #include "ReloadGunAbility.generated.h"
 
-/**
- * 
- */
+class AAmmunition;
+
 UCLASS()
 class EVIDENCE_API UReloadGunAbility : public UEIGameplayAbility
 {
@@ -16,5 +15,12 @@ class EVIDENCE_API UReloadGunAbility : public UEIGameplayAbility
 
 public:
 	UReloadGunAbility();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AAmmunition> AmmoClass;
 	
 };
