d97d7199eb4f89d7111f447974ae0b2f9a12df4d
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-04-26 17:20:09 +0100

    Created tracker dart class

---
+++ Source/Evidence/Items/Equipment/Ammunition/TrackerDart.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TrackerDart.h"
+
+ATrackerDart::ATrackerDart()
+{
+	EquipmentName = FString("Tracker dart");
+}
---
+++ Source/Evidence/Items/Equipment/Ammunition/TrackerDart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ammunition.h"
+#include "TrackerDart.generated.h"
+
+UCLASS()
+class EVIDENCE_API ATrackerDart : public AAmmunition
+{
+	GENERATED_BODY()
+
+public:
+	ATrackerDart();
+	
+};
