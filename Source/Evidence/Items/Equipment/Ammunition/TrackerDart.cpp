0a4bc6cf5b3da7adb45c898df135a5a7e465aa97
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-04-28 00:20:24 +0100

    Created tracker gun and dart blueprints

--- Source/Evidence/Items/Equipment/Ammunition/TrackerDart.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "TrackerDart.h"
-
-ATrackerDart::ATrackerDart()
-{
-	EquipmentName = FString("Tracker dart");
-}
