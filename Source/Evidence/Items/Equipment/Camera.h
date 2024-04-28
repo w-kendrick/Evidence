c1993950ec688f87dee82783bf8a3826ab8cca20
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-04-28 21:57:30 +0100

    Created camera class

---
+++ Source/Evidence/Items/Equipment/Camera.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Items/Equipment.h"
+#include "Camera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API ACamera : public AEquipment
+{
+	GENERATED_BODY()
+
+public:
+	ACamera();
+	
+};
