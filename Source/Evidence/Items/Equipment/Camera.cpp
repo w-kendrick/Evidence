14018e112d331507d044ae57cfb2256c61005384
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-01 21:12:56 +0100

    Added scene capture component to camera

--- Source/Evidence/Items/Equipment/Camera.cpp
+++ Source/Evidence/Items/Equipment/Camera.cpp
@@ -2,8 +2,55 @@
 
 
 #include "Camera.h"
+#include "Net/UnrealNetwork.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Kismet/KismetRenderingLibrary.h"
 
 ACamera::ACamera()
 {
+	MaxPhotos = 10;
 
+	Photos.SetNum(MaxPhotos);
+	for (int i = 0; i < MaxPhotos; i++)
+	{
+		Photos.Add(nullptr);
+	}
+
+	SceneCaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("SceneCaptureComponent"));
+}
+
+void ACamera::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetRemainingPhotos(MaxPhotos);
+}
+
+void ACamera::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACamera, RemainingPhotos);
+}
+
+void ACamera::TakePhoto()
+{
+	UTextureRenderTarget2D* Target = UKismetRenderingLibrary::CreateRenderTarget2D(GetWorld(), 1920, 1080);
+	SceneCaptureComponent->TextureTarget = Target;
+	SceneCaptureComponent->CaptureScene();
+
+	Photos[Photos.Num() - RemainingPhotos] = Target;
+
+	SetRemainingPhotos(RemainingPhotos - 1);
+}
+
+void ACamera::SetRemainingPhotos(const uint8 Remaining)
+{
+	RemainingPhotos = Remaining;
+	OnPhotosChanged.Broadcast(RemainingPhotos);
+}
+
+void ACamera::OnRep_RemainingPhotos()
+{
+	OnPhotosChanged.Broadcast(RemainingPhotos);
 }
--- Source/Evidence/Items/Equipment/Camera.h
+++ Source/Evidence/Items/Equipment/Camera.h
@@ -6,9 +6,10 @@
 #include "Evidence/Items/Equipment.h"
 #include "Camera.generated.h"
 
-/**
- * 
- */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPhotosChanged, const uint8);
+
+class USceneCaptureComponent2D;
+
 UCLASS()
 class EVIDENCE_API ACamera : public AEquipment
 {
@@ -16,5 +17,31 @@ class EVIDENCE_API ACamera : public AEquipment
 
 public:
 	ACamera();
+
+	FOnPhotosChanged OnPhotosChanged;
+
+	void TakePhoto();
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly)
+	USceneCaptureComponent2D* SceneCaptureComponent;
+
+	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+
+	UPROPERTY(EditDefaultsOnly)
+	uint8 MaxPhotos;
+
+	UPROPERTY(ReplicatedUsing=OnRep_RemainingPhotos)
+	uint8 RemainingPhotos;
+
+	UFUNCTION()
+	void OnRep_RemainingPhotos();
+
+	void SetRemainingPhotos(const uint8 Remaining);
+
+private:
+	UPROPERTY()
+	TArray<UTextureRenderTarget2D*> Photos;
 	
 };
