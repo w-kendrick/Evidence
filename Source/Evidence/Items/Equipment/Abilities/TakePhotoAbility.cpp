d4a7e3b6caf23412322d24209fee1fdc4317f357
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-01 22:11:35 +0100

    Created take photo ability class

---
+++ Source/Evidence/Items/Equipment/Abilities/TakePhotoAbility.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TakePhotoAbility.h"
+
+UTakePhotoAbility::UTakePhotoAbility()
+{
+	AbilityInputID = EAbilityInputID::Use;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Use")));
+}
---
+++ Source/Evidence/Items/Equipment/Abilities/TakePhotoAbility.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Character/Abilities/EIGameplayAbility.h"
+#include "TakePhotoAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API UTakePhotoAbility : public UEIGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UTakePhotoAbility();
+	
+};
