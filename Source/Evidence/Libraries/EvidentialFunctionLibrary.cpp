aaea8a8581efbe5a66a10f153035a0a759ca63d1
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-10 23:59:52 +0100

    Created evidential function library

---
+++ Source/Evidence/Libraries/EvidentialFunctionLibrary.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EvidentialFunctionLibrary.h"
+#include "Evidence/Structs/EvidentialInfo.h"
+
+float UEvidentialFunctionLibrary::CalculateCash(const TArray<FEvidentialInfo> InfoArray)
+{
+	return 0.0f;
+}
---
+++ Source/Evidence/Libraries/EvidentialFunctionLibrary.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "EvidentialFunctionLibrary.generated.h"
+
+struct FEvidentialInfo;
+
+UCLASS()
+class EVIDENCE_API UEvidentialFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	static float CalculateCash(const TArray<FEvidentialInfo> InfoArray);
+};
