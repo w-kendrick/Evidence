5fb775e51fc87a95f2a96cc6ad362d29bf8ac77b
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-10 20:42:18 +0100

    Created evidential struct

--- Source/Evidence/Structs/EvidentialInfo.h
+++ Source/Evidence/Structs/EvidentialInfo.h
@@ -3,3 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Evidence/Enums/EvidentialType.h"
+#include "Evidence/Enums/EvidentialMedium.h"
+#include "EvidentialInfo.generated.h"
+
+USTRUCT()
+struct FEvidentialInfo
+{
+	GENERATED_BODY();
+
+	EEvidentialType Type;
+	EEvidentialMedium Medium;
+	float BaseCash;
+
+	FEvidentialInfo(const EEvidentialType NewType, const EEvidentialMedium NewMedium, const float NewCash)
+		: Type(NewType), Medium(NewMedium), BaseCash(NewCash)
+	{
+
+	}
+
+	FEvidentialInfo() {}
+};
