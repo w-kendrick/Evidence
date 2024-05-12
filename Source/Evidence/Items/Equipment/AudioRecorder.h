49d93297c1183b88f19c4418098b934899179e28
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-12 22:01:23 +0100

    Created audio recorder file

---
+++ Source/Evidence/Items/Equipment/AudioRecorder.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Items/Equipment.h"
+#include "AudioRecorder.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AAudioRecorder : public AEquipment
+{
+	GENERATED_BODY()
+
+public:
+	AAudioRecorder();
+	
+};
