4839cfb8a779003e746af0e058ac2627ca809873
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-28 19:55:08 +0100

    Made alien character an evidential

--- Source/Evidence/Enemies/Alien/AlienCharacter.cpp
+++ Source/Evidence/Enemies/Alien/AlienCharacter.cpp
@@ -7,4 +7,22 @@
 AAlienCharacter::AAlienCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+	WorthAlive = 1000.f;
+	WorthDead = 500.f;
+}
+
+float AAlienCharacter::GetBaseWorth() const
+{
+	if (IsAlive())
+	{
+		return WorthAlive;
+	}
+	{
+		return WorthDead;
+	}
+}
+
+EEvidentialType AAlienCharacter::GetType() const
+{
+	return EEvidentialType::Alien;
 }
