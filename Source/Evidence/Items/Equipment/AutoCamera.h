eb838b7fa1544bb2ed1eb6816f7fd5b00b04bf7a
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-05-30 21:07:59 +0100

    Created auto camera class

---
+++ Source/Evidence/Items/Equipment/AutoCamera.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Items/Equipment/PhotoCamera.h"
+#include "AutoCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AAutoCamera : public APhotoCamera
+{
+	GENERATED_BODY()
+
+public:
+	AAutoCamera();
+	
+};
