384f87f574410e7b1334392595aff6c05c100984
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-06-01 18:35:35 +0100

    Added example values to worth map

--- Source/Evidence/Libraries/EvidentialFunctionLibrary.cpp
+++ Source/Evidence/Libraries/EvidentialFunctionLibrary.cpp
@@ -5,8 +5,31 @@
 #include "Evidence/Structs/EvidentialInfo.h"
 
 TMap<TPair<EEvidentialType, EEvidentialMedium>, float> WorthMap = {
-	{{EEvidentialType::Alien, EEvidentialMedium::Photo}, 1.0f},
-	{{EEvidentialType::Alien, EEvidentialMedium::Audio}, 5.0f}
+	{{EEvidentialType::Alien, EEvidentialMedium::Photo}, 5.0f},
+	{{EEvidentialType::Alien, EEvidentialMedium::Audio}, 5.0f},
+	{{EEvidentialType::Alien, EEvidentialMedium::Video}, 7.5f},
+	{{EEvidentialType::Alien, EEvidentialMedium::Item}, 1000.0f},
+	{{EEvidentialType::UFO, EEvidentialMedium::Photo}, 3.0f},
+	{{EEvidentialType::UFO, EEvidentialMedium::Audio}, 1.0f},
+	{{EEvidentialType::UFO, EEvidentialMedium::Video}, 10.0f},
+	{{EEvidentialType::UFO, EEvidentialMedium::Item}, 1000000.0f},
+	{{EEvidentialType::Victim, EEvidentialMedium::Photo}, 3.0f},
+	{{EEvidentialType::Victim, EEvidentialMedium::Audio}, 1.0f},
+	{{EEvidentialType::Victim, EEvidentialMedium::Video}, 10.0f},
+	{{EEvidentialType::Victim, EEvidentialMedium::Item}, 1000000.0f},
+	{{EEvidentialType::Tech, EEvidentialMedium::Photo}, 3.0f},
+	{{EEvidentialType::Tech, EEvidentialMedium::Audio}, 1.0f},
+	{{EEvidentialType::Tech, EEvidentialMedium::Video}, 10.0f},
+	{{EEvidentialType::Tech, EEvidentialMedium::Item}, 1000000.0f},
+	{{EEvidentialType::Prints, EEvidentialMedium::Photo}, 3.0f},
+	{{EEvidentialType::Prints, EEvidentialMedium::Audio}, 1.0f},
+	{{EEvidentialType::Prints, EEvidentialMedium::Video}, 10.0f},
+	{{EEvidentialType::Prints, EEvidentialMedium::Item}, 1000000.0f},
+	{{EEvidentialType::Blood, EEvidentialMedium::Photo}, 3.0f},
+	{{EEvidentialType::Blood, EEvidentialMedium::Audio}, 1.0f},
+	{{EEvidentialType::Blood, EEvidentialMedium::Video}, 10.0f},
+	{{EEvidentialType::Blood, EEvidentialMedium::Item}, 1000000.0f}
+
 };
 
 float UEvidentialFunctionLibrary::CalculateCash(const TArray<FEvidentialCapture>& CaptureArray)
