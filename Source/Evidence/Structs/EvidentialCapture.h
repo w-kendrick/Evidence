e4ae23a232b9cd416a878790b66d076a200fac14
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-06-01 18:53:02 +0100

    Implemented function to get evidential capture worth

--- Source/Evidence/Structs/EvidentialCapture.h
+++ Source/Evidence/Structs/EvidentialCapture.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Evidence/Enums/EvidentialMedium.h"
 #include "Evidence/Structs/EvidentialInfo.h"
+#include "Evidence/Libraries/EvidentialFunctionLibrary.h"
 #include "EvidentialCapture.generated.h"
 
 USTRUCT()
@@ -33,9 +34,27 @@ struct FEvidentialCapture
 
 		for (const FEvidentialInfo& Info : Infos)
 		{
-
+			Worth += UEvidentialFunctionLibrary::GetWorth(Info.Type, Medium);
 		}
 
+		Worth += GetVarietyBonus();
+
 		return Worth;
+	}
+
+	float GetVarietyBonus() const
+	{
+		constexpr float BonusPerType = 10.f;
+		TArray<EEvidentialType> Types;
+
+		for (const FEvidentialInfo& Info : Infos)
+		{
+			if (!Types.Contains(Info.Type))
+			{
+				Types.Add(Info.Type);
+			}
+		}
+
+		return Types.Num() * BonusPerType;
 	}
 };
