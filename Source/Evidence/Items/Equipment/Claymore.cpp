f53d055a33bb114622818e236d84e373c7a8fc9b
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-06-02 13:28:11 +0100

    Added perception component to claymore

--- Source/Evidence/Items/Equipment/Claymore.cpp
+++ Source/Evidence/Items/Equipment/Claymore.cpp
@@ -2,8 +2,52 @@
 
 
 #include "Claymore.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 AClaymore::AClaymore()
 {
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
 
+	Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight"));
+	Sight->PeripheralVisionAngleDegrees = 45.f;
+	Sight->SightRadius = 200.f;
+	Sight->LoseSightRadius = 200.f;
+	Sight->DetectionByAffiliation.bDetectEnemies = true;
+	Sight->DetectionByAffiliation.bDetectNeutrals = true;
+	Sight->DetectionByAffiliation.bDetectFriendlies = true;
+
+	PerceptionComponent->ConfigureSense(*Sight);
+}
+
+void AClaymore::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ThisClass::OnTargetPerceptionUpdated);
+	}
+}
+
+void AClaymore::Plant()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "Plant claymore");
+}
+
+void AClaymore::GetActorEyesViewPoint(FVector& OutLocation, FRotator& OutRotation) const
+{
+	FRotator Rot;
+	Super::GetActorEyesViewPoint(OutLocation, Rot);
+
+	OutRotation = GetActorRightVector().Rotation();
+}
+
+void AClaymore::OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	Detonate();
+}
+
+void AClaymore::Detonate()
+{
 }
--- Source/Evidence/Items/Equipment/Claymore.h
+++ Source/Evidence/Items/Equipment/Claymore.h
@@ -6,6 +6,9 @@
 #include "Evidence/Items/Equipment/PlantableEquipment.h"
 #include "Claymore.generated.h"
 
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+
 UCLASS()
 class EVIDENCE_API AClaymore : public APlantableEquipment
 {
@@ -13,5 +16,23 @@ class EVIDENCE_API AClaymore : public APlantableEquipment
 
 public:
 	AClaymore();
+
+	virtual void Plant() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	UAIPerceptionComponent* PerceptionComponent;
+
+	UPROPERTY()
+	UAISenseConfig_Sight* Sight;
+
+	virtual void GetActorEyesViewPoint(FVector& OutLocation, FRotator& OutRotation) const override;
+
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+	void Detonate();
 	
 };
