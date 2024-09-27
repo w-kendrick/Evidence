fe37e9fe88c22106fb09aa83d182d1d4e0a8a883
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-09-27 17:54:58 +0100

    Added world generators

---
+++ Source/Evidence/Items/WorldGenerator.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldGenerator.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+AWorldGenerator::AWorldGenerator()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = Box;
+	Box->bHiddenInGame = false;
+	Box->SetVisibility(true);
+}
+
+void AWorldGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (HasAuthority())
+	{
+		SpawnClasses();
+	}
+}
+
+void AWorldGenerator::SpawnClasses()
+{
+	for (const FEvidentialSpawn& SpawnClass : ClassesToSpawn)
+	{
+		const TSubclassOf<AActor>& Class = SpawnClass.Class;
+		const float& Rate = SpawnClass.SpawnRate;
+
+		const float& Area = Box->GetUnscaledBoxExtent().X * Box->GetUnscaledBoxExtent().Y;
+
+		const int32 Count = FMath::RoundToInt(Area * Rate * 0.01f);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, FString::FromInt(Count));
+
+		for (uint8 I = 0; I < Count; I++)
+		{
+			const FVector Location = UKismetMathLibrary::RandomPointInBoundingBox(Box->GetComponentLocation(), Box->GetScaledBoxExtent());
+			SpawnClassAtLocation(Class, Location);
+		}
+	}
+}
+
+void AWorldGenerator::SpawnClassAtLocation(const TSubclassOf<AActor>& Class, const FVector& Location)
+{
+	FVector GroundPoint;
+
+	if (GetGroundPoint(Location, GroundPoint))
+	{
+		const FTransform& Transform = FTransform(GroundPoint);
+
+		FActorSpawnParameters Params;
+
+		AActor* const Spawned = GetWorld()->SpawnActor<AActor>(Class, Transform, Params);
+	}
+}
+
+bool AWorldGenerator::GetGroundPoint(const FVector& OriginalPoint, FVector& NewPoint) const
+{
+	bool bResult = false;
+	NewPoint = OriginalPoint;
+
+	FHitResult Hit;
+	const FVector EndPoint = OriginalPoint - 10 * FVector::UpVector * Box->GetScaledBoxExtent().Z;
+	if (GetWorld()->LineTraceSingleByChannel(Hit, OriginalPoint, EndPoint, ECollisionChannel::ECC_Visibility))
+	{
+		NewPoint = Hit.ImpactPoint;
+		bResult = true;
+	}
+
+	return bResult;
+}
