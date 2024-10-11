d8c5e68d9d1b183c3b9d8612618818d7b9f8ab28
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-10-11 22:08:20 +0100

    Fail example

--- Plugins/ProneMovementMode/Source/ProneMovementMode/Private/ProneCharacter.cpp
+++ Plugins/ProneMovementMode/Source/ProneMovementMode/Private/ProneCharacter.cpp
@@ -13,6 +13,15 @@
 #include "AttributeSets/StaminaSet.h"
 #include "AttributeSets/MovementSet.h"
 
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ProneCharacter)
+
 AProneCharacter::AProneCharacter(const FObjectInitializer& FObjectInitializer)
 	: Super(FObjectInitializer.SetDefaultSubobjectClass<UProneMovement>(CharacterMovementComponentName))
 {
