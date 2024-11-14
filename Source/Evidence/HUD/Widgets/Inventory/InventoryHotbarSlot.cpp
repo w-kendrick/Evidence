9a741381703b8bd53c77d96f11626770de1dd07c
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-14 22:12:56 +0000

    Added inventory component pointer and index to inventory hotbar slot widget

--- Source/Evidence/HUD/Widgets/Inventory/InventoryHotbarSlot.cpp
+++ Source/Evidence/HUD/Widgets/Inventory/InventoryHotbarSlot.cpp
@@ -4,14 +4,29 @@
 #include "InventoryHotbarSlot.h"
 #include "Components/TextBlock.h"
 #include "Components/Image.h"
+#include "Evidence/Character/BaseCharacter.h"
 #include "Evidence/Items/Equipment.h"
 
 void UInventoryHotbarSlot::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	if (GetOwningPlayer())
+	{
+		ABaseCharacter* const Character = Cast<ABaseCharacter>(GetOwningPlayer()->GetCharacter());
+		if (Character)
+		{
+			InventoryComponent = Character->GetInventoryComponent();
+		}
+	}
+
 	UpdateSlot(nullptr);
 	SelectSlot(false);
+}
+
+void UInventoryHotbarSlot::SetIndex(const uint8 Index)
+{
+	InventoryIndex = Index;
 }
 
 void UInventoryHotbarSlot::UpdateSlot(const AEquipment* const Equipment)
