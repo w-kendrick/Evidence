96c8112c3568320c19b52e1743f5d0510c5a1025
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-15 18:28:44 +0000

    Added dragging for locker slots in locker widget

--- Source/Evidence/HUD/Widgets/Locker/LockerSlotWidget.cpp
+++ Source/Evidence/HUD/Widgets/Locker/LockerSlotWidget.cpp
@@ -2,14 +2,47 @@
 
 
 #include "LockerSlotWidget.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
 #include "Components/TextBlock.h"
 #include "Evidence/Items/Equipment.h"
+#include "Kismet/GameplayStatics.h"
+#include "Evidence/Hub/Locker.h"
+#include "Evidence/HUD/Widgets/Locker/Drag/LockerSlotDragPreview.h"
+#include "Evidence/HUD/Widgets/Locker/Drag/LockerSlotDragDropOperation.h"
 
 void ULockerSlotWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	Locker = Cast<ALocker>(UGameplayStatics::GetActorOfClass(GetWorld(), ALocker::StaticClass()));
+
 	UpdateSlot(nullptr);
+}
+
+FReply ULockerSlotWidget::NativeOnPreviewMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	if (InMouseEvent.IsMouseButtonDown(LeftMouseButton) && Locker) //&& Locker->GetEquipmentAtIndex(InventoryIndex))
+	{
+		return UWidgetBlueprintLibrary::DetectDragIfPressed(InMouseEvent, this, LeftMouseButton).NativeReply;
+	}
+	return Super::NativeOnPreviewMouseButtonDown(InGeometry, InMouseEvent);
+}
+
+void ULockerSlotWidget::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation)
+{
+	Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
+
+	ULockerSlotDragPreview* DragPreview = CreateWidget<ULockerSlotDragPreview>(this, DragPreviewClass);
+	DragPreview->SetIndex(StorageIndex);
+	DragPreview->SetLocker(Locker);
+	DragPreview->SetColour(FColor::White);
+
+	ULockerSlotDragDropOperation* DragOperation = Cast<ULockerSlotDragDropOperation>(UWidgetBlueprintLibrary::CreateDragDropOperation(DragOperationClass));
+	DragOperation->DefaultDragVisual = DragPreview;
+	DragOperation->Pivot = EDragPivot::MouseDown;
+	DragOperation->SpawnInitialize(StorageIndex, Locker);
+
+	OutOperation = DragOperation;
 }
 
 void ULockerSlotWidget::SetIndex(const uint8 NewIndex)
