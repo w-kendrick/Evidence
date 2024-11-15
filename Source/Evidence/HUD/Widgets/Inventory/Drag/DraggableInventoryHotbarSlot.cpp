179053ccd925eac8120b1916dea3c70eaf6ee271
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-15 15:44:41 +0000

    Revert "Moved inventory index to draggable inventory slot widget"
    This reverts commit d51c097856598f569a5074d696d416c538d1cd59.

--- Source/Evidence/HUD/Widgets/Inventory/Drag/DraggableInventoryHotbarSlot.cpp
+++ Source/Evidence/HUD/Widgets/Inventory/Drag/DraggableInventoryHotbarSlot.cpp
@@ -7,25 +7,6 @@
 #include "InventorySlotDragDropOperation.h"
 #include "Evidence/Character/Components/InventoryManagerComponent.h"
 
-void UDraggableInventoryHotbarSlot::NativeConstruct()
-{
-	Super::NativeConstruct();
-
-	if (GetOwningPlayer())
-	{
-		ABaseCharacter* const Character = Cast<ABaseCharacter>(GetOwningPlayer()->GetCharacter());
-		if (Character)
-		{
-			InventoryComponent = Character->GetInventoryComponent();
-		}
-	}
-}
-
-void UDraggableInventoryHotbarSlot::SetIndex(const uint8 Index)
-{
-	InventoryIndex = Index;
-}
-
 FReply UDraggableInventoryHotbarSlot::NativeOnPreviewMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
 {
 	if (InMouseEvent.IsMouseButtonDown(LeftMouseButton) && InventoryComponent && InventoryComponent->GetEquipmentAtIndex(InventoryIndex))
@@ -37,17 +18,5 @@ FReply UDraggableInventoryHotbarSlot::NativeOnPreviewMouseButtonDown(const FGeom
 
 void UDraggableInventoryHotbarSlot::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation)
 {
-	Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
-
-	UInventorySlotDragPreview* DragPreview = CreateWidget<UInventorySlotDragPreview>(this, DragPreviewClass);
-	DragPreview->SetIndex(InventoryIndex);
-	DragPreview->SetInventoryComponent(InventoryComponent);
-	DragPreview->SetColour(FColor::Black);
-
-	UInventorySlotDragDropOperation* DragWidget = Cast<UInventorySlotDragDropOperation>(UWidgetBlueprintLibrary::CreateDragDropOperation(DragWidgetClass));
-	DragWidget->DefaultDragVisual = DragPreview;
-	DragWidget->Pivot = EDragPivot::MouseDown;
-	DragWidget->SpawnInitialize(InventoryIndex, InventoryComponent);
-
-	OutOperation = DragWidget;
+	
 }
