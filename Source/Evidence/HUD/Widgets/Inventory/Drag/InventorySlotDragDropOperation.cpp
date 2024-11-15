f27e7b4a7249ae24430f193969386318da19c6d2
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-15 19:55:18 +0000

    Implemented inventory slot on locker slot drag and drop

--- Source/Evidence/HUD/Widgets/Inventory/Drag/InventorySlotDragDropOperation.cpp
+++ Source/Evidence/HUD/Widgets/Inventory/Drag/InventorySlotDragDropOperation.cpp
@@ -3,8 +3,9 @@
 
 #include "InventorySlotDragDropOperation.h"
 
-void UInventorySlotDragDropOperation::SpawnInitialize(const uint8 NewIndex, UInventoryManagerComponent* Comp)
+void UInventorySlotDragDropOperation::SpawnInitialize(const uint8 NewIndex, UInventoryManagerComponent* Comp, UDraggableInventoryHotbarSlot* Source)
 {
 	Index = NewIndex;
 	InventoryComponent = Comp;
+	WidgetSource = Source;
 }
