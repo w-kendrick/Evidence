377bb82947ebb8499f035076d32c7bf3f4ea46b6
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-16 13:33:25 +0000

    Added input blocking tag on death

--- Plugins/AbilityInputSystem/Source/AbilityInputSystem/Private/BasicGameplayTags.cpp
+++ Plugins/AbilityInputSystem/Source/AbilityInputSystem/Private/BasicGameplayTags.cpp
@@ -11,5 +11,6 @@ namespace BasicGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Look_Mouse, "InputTag.Look.Mouse", "Look (mouse) input.");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Jump, "InputTag.Jump", "Jump input.");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Crouch, "InputTag.Crouch", "Crouch input.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputBlocked, "Gameplay.InputBlocked", "Input for pawn is blocked");
 }
 
