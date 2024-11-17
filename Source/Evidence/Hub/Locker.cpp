101579390e8e26b0d3ef28b04aa28e949b66d059
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-17 19:49:03 +0000

    Fixed equipment wipe crashes

--- Source/Evidence/Hub/Locker.cpp
+++ Source/Evidence/Hub/Locker.cpp
@@ -38,7 +38,11 @@ FString ALocker::GetInteractionString_Implementation()
 
 void ALocker::SetEquipmentAtIndex(AEquipment* const Equipment, const uint8 Index)
 {
-	Equipment->SetOwner(this);
+	if (Equipment)
+	{
+		Equipment->SetOwner(this);
+	}
+
 	Storage.AddEntry(Equipment, Index);
 	OnLockerStorageChanged.Broadcast(Storage);
 }
