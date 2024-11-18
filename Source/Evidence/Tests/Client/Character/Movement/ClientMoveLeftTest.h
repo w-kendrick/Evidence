575a133095eb605f3cc46a65bbb0aed957c2d67f
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-18 22:18:32 +0000

    Created client move left test

---
+++ Source/Evidence/Tests/Client/Character/Movement/ClientMoveLeftTest.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/Client/Character/ClientCharacterTest.h"
+#include "ClientMoveLeftTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AClientMoveLeftTest : public AClientCharacterTest
+{
+	GENERATED_BODY()
+
+public:
+	AClientMoveLeftTest();
+
+	void StartTest() override;
+
+protected:
+	void OnTrigger1BeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+	void OnTrigger2BeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* MoveAction;
+	
+};
