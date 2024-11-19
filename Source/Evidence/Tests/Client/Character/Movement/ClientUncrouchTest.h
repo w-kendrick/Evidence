b5bc07fdfc8e1c6bfaf087bce57e536744b92945
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-19 19:29:48 +0000

    Created uncrouch test

---
+++ Source/Evidence/Tests/Client/Character/Movement/ClientUncrouchTest.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/Client/Character/ClientCharacterTest.h"
+#include "ClientUncrouchTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AClientUncrouchTest : public AClientCharacterTest
+{
+	GENERATED_BODY()
+
+public:
+	AClientUncrouchTest();
+
+	void StartTest() override;
+
+protected:
+	void OnTrigger1BeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+	void OnTrigger2BeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* CrouchAction;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* MoveAction;
+
+private:
+	void Step2();
+	
+};
