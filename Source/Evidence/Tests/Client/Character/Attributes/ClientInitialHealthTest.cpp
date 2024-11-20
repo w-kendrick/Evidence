5381448c0a991d7d47758037c0ae4d661677aea9
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-20 22:56:01 +0000

    Updated initial health test to run in listen server mode

--- Source/Evidence/Tests/Client/Character/Attributes/ClientInitialHealthTest.cpp
+++ Source/Evidence/Tests/Client/Character/Attributes/ClientInitialHealthTest.cpp
@@ -14,7 +14,21 @@ void AClientInitialHealthTest::StartTest()
 	const bool Client1_Result = (Client1TestInfo.MyCharacter->GetHealth() == 100.f) && (Client1TestInfo.MyCharacter->GetMaxHealth() == 100.f);
 	const bool Client2_Result = (Client2TestInfo.MyCharacter->GetHealth() == 100.f) && (Client2TestInfo.MyCharacter->GetMaxHealth() == 100.f);
 
-	if (Client1_Result && Client2_Result)
+	bool Server_Result = true;
+
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABaseCharacter::StaticClass(), Actors);
+	for (AActor* Actor : Actors)
+	{
+		ABaseCharacter* Character = Cast<ABaseCharacter>(Actor);
+
+		if (Character)
+		{
+			Server_Result &= (Character->GetHealth() == 100.f) && (Character->GetMaxHealth() == 100.f);
+		}
+	}
+
+	if (Server_Result && Client1_Result && Client2_Result)
 	{
 		FinishTest(EFunctionalTestResult::Succeeded, FString("Passed"));
 	}
