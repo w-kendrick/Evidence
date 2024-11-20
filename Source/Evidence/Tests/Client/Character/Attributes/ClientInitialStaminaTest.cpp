197bf7fafc494972fe5e8c561d2529565633bef1
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-20 22:13:48 +0000

    Updated initial stamina test to run in listen server mode

--- Source/Evidence/Tests/Client/Character/Attributes/ClientInitialStaminaTest.cpp
+++ Source/Evidence/Tests/Client/Character/Attributes/ClientInitialStaminaTest.cpp
@@ -14,7 +14,21 @@ void AClientInitialStaminaTest::StartTest()
 	const bool Client1_Result = (Client1TestInfo.MyCharacter->GetStamina() == 100.f) && (Client1TestInfo.MyCharacter->GetMaxStamina() == 100.f);
 	const bool Client2_Result = (Client2TestInfo.MyCharacter->GetStamina() == 100.f) && (Client2TestInfo.MyCharacter->GetMaxStamina() == 100.f);
 
-	if (Client1_Result && Client2_Result)
+	bool Server_Result = true;
+
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABaseCharacter::StaticClass(), Actors);
+	for (AActor* Actor : Actors)
+	{
+		ABaseCharacter* Character = Cast<ABaseCharacter>(Actor);
+
+		if (Character)
+		{
+			Server_Result &= (Character->GetStamina() == 100.f) && (Character->GetMaxStamina() == 100.f);
+		}
+	}
+
+	if (Server_Result && Client1_Result && Client2_Result)
 	{
 		FinishTest(EFunctionalTestResult::Succeeded, FString("Passed"));
 	}
