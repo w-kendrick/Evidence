99c8bc1a0609eb992597b90cc90bd9081348a1cc
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-20 19:08:36 +0000

    Created client inventory hotbar quick select test

---
+++ Source/Evidence/Tests/Client/Character/Inventory/ClientHotbarQuickSelectTest.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/Client/Character/ClientCharacterTest.h"
+#include "ClientHotbarQuickSelectTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AClientHotbarQuickSelectTest : public AClientCharacterTest
+{
+	GENERATED_BODY()
+
+public:
+	AClientHotbarQuickSelectTest();
+
+	void StartTest() override;
+	void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar1Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar2Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar3Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar4Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar5Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar6Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar7Action;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* Hotbar8Action;
+
+	int32 ExpectedIndex;
+	bool bFirstCycle;
+
+	void Cycle();
+
+	UInputAction* ChooseInputAction() const;
+	
+};
