5353fe802316e8d7d6adc724fd48f31fb4798970
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-22 15:37:32 +0000

    Updated inventory size test to use listen server mode

--- Source/Evidence/Tests/Client/Character/Inventory/ClientInventorySizeTest.cpp
+++ Source/Evidence/Tests/Client/Character/Inventory/ClientInventorySizeTest.cpp
@@ -12,8 +12,17 @@ void AClientInventorySizeTest::StartTest()
 {
 	Super::StartTest();
 
-	bClient1Passed = (Client1TestInfo.OtherClientCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
-	bClient2Passed = (Client2TestInfo.MyCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bServerPassed = (ServerTestInfo.MyCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bClient1Passed = (Client1TestInfo.ServerCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bClient2Passed = (Client2TestInfo.ServerCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+
+	bServerPassed &= (ServerTestInfo.Client1Character->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bClient1Passed &= (Client1TestInfo.MyCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bClient2Passed &= (Client2TestInfo.OtherClientCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+
+	bServerPassed &= (ServerTestInfo.Client2Character->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bClient1Passed &= (Client1TestInfo.OtherClientCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
+	bClient2Passed &= (Client2TestInfo.MyCharacter->GetInventoryComponent()->GetInventory().GetNum() == UInventoryManagerComponent::INVENTORY_SIZE);
 
 	CheckResult();
 }
