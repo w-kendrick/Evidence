9ad4644dfde49201e2426589f3384ed1a3a8cc8e
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-22 18:48:25 +0000

    Adapted client move forward test

--- Source/Evidence/Tests/Character/Movement/ClientMoveForwardTest.cpp
+++ Source/Evidence/Tests/Character/Movement/ClientMoveForwardTest.cpp
@@ -17,11 +17,22 @@ void AClientMoveForwardTest::StartTest()
 	StartInjectContinuousInput(Client2TestInfo.InputSubsystem, MoveAction, FVector(0, 1, 0));
 }
 
+void AClientMoveForwardTest::OnServerTriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	const ABaseCharacter* const Character = Cast<ABaseCharacter>(OtherActor);
+
+	if (Character == ServerTestInfo.Client2Character)
+	{
+		bServerPassed = true;
+		CheckResult();
+	}
+}
+
 void AClientMoveForwardTest::OnClient1TriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
 {
 	const ABaseCharacter* const Character = Cast<ABaseCharacter>(OtherActor);
 
-	if (Character)
+	if (Character == Client1TestInfo.OtherClientCharacter)
 	{
 		bClient1Passed = true;
 		CheckResult();
@@ -32,7 +43,7 @@ void AClientMoveForwardTest::OnClient2TriggerBeginOverlap(AActor* OverlappedActo
 {
 	const ABaseCharacter* const Character = Cast<ABaseCharacter>(OtherActor);
 
-	if (Character)
+	if (Character == Client2TestInfo.MyCharacter)
 	{
 		bClient2Passed = true;
 		CheckResult();
