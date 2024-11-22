dc38df4572d4913d7fa38c3b83da750ad76ec64e
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-22 18:40:53 +0000

    Created server move backward test

---
+++ Source/Evidence/Tests/Character/Movement/ServerMoveBackwardTest.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/Character/CharacterTest.h"
+#include "ServerMoveBackwardTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AServerMoveBackwardTest : public ACharacterTest
+{
+	GENERATED_BODY()
+	
+public:
+	AServerMoveBackwardTest();
+
+	void StartTest() override;
+
+protected:
+	void OnServerTriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+	void OnClient1TriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+	void OnClient2TriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* MoveAction;
+};
