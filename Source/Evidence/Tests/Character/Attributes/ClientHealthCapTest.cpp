b71a119703f1763ebf8887a2199ed1a8a03c51ef
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-22 21:26:13 +0000

    Created client health cap test

---
+++ Source/Evidence/Tests/Character/Attributes/ClientHealthCapTest.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClientHealthCapTest.h"
+#include "AdvancedAbilityComponent.h"
+
+AClientHealthCapTest::AClientHealthCapTest()
+{
+	TimeLimit = 5.0f;
+	TimesUpResult = EFunctionalTestResult::Succeeded;
+}
+
+void AClientHealthCapTest::StartTest()
+{
+	Super::StartTest();
+
+	UAdvancedAbilityComponent* AbilityComponent = Client1TestInfo.MyCharacter->GetComponentByClass<UAdvancedAbilityComponent>();
+
+	if (AbilityComponent)
+	{
+		const FGameplayEffectSpecHandle SpecHandle = AbilityComponent->MakeOutgoingSpec(AwardHealthEffectClass, 1, AbilityComponent->MakeEffectContext());
+		AbilityComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data);
+	}
+}
+
+void AClientHealthCapTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bTestHasBegun)
+	{
+		if (Client1TestInfo.MyCharacter->GetHealth() > MAX_HEALTH || ServerTestInfo.Client1Character->GetHealth() > MAX_HEALTH)
+		{
+			FinishTest(EFunctionalTestResult::Failed, FString("Failed"));
+		}
+	}
+}
---
+++ Source/Evidence/Tests/Character/Attributes/ClientHealthCapTest.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/Character/CharacterTest.h"
+#include "ClientHealthCapTest.generated.h"
+
+class UGameplayEffect;
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AClientHealthCapTest : public ACharacterTest
+{
+	GENERATED_BODY()
+
+public:
+	AClientHealthCapTest();
+
+	void StartTest() override;
+	void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UGameplayEffect> AwardHealthEffectClass;
+
+	static constexpr float MAX_HEALTH = 100.f;
+	
+};
