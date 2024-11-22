3c2261771dea839114604b3c5385706c69c7e76e
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-22 18:24:56 +0000

    Created server jump test

---
+++ Source/Evidence/Tests/Character/Movement/ServerJumpTest.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/Character/CharacterTest.h"
+#include "ServerJumpTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AServerJumpTest : public ACharacterTest
+{
+	GENERATED_BODY()
+
+public:
+	AServerJumpTest();
+
+	void StartTest() override;
+
+protected:
+	void OnServerTriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+	void OnClient1TriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+	void OnClient2TriggerBeginOverlap(AActor* OverlappedActor, AActor* OtherActor) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* JumpAction;
+
+	
+};
