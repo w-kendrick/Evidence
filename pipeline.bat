b815162d093bd1380aa3d71f470890b560472820
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-24 12:59:59 +0000

    Added network emulation runs to pipeline script

--- pipeline.bat
+++ pipeline.bat
@@ -4,7 +4,30 @@ setlocal
 set UNREAL_EDITOR="C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
 set PROJECT_FILE="C:\Users\willk\Documents\Unreal Projects\Evidence\Evidence.uproject"
 
+echo ================================================================================================================================================
+echo Running tests in perfect network conditions
+echo ================================================================================================================================================
+
 %UNREAL_EDITOR% %PROJECT_FILE% -Deterministic -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ini:EditorPerProjectUserSettings:[/Script/UnrealEd.LevelEditorPlaySettings]:PlayNetMode=PIE_ListenServer -ExecCmds="Automation RunTests Project.Functional Tests.Tests;quit" -StdOut -LogCmds="Global Warning, LogCheckSubobjects Fatal, LogAutomationController Verbose"
+
+echo ================================================================================================================================================
+echo Running tests with 5% packet loss
+echo ================================================================================================================================================
+
+%UNREAL_EDITOR% %PROJECT_FILE% -Deterministic -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ini:Engine:[PacketSimulationSettings]:PktLoss=5 -ini:EditorPerProjectUserSettings:[/Script/UnrealEd.LevelEditorPlaySettings]:PlayNetMode=PIE_ListenServer -ExecCmds="Automation RunTests Project.Functional Tests.Tests;quit" -StdOut -LogCmds="Global Warning, LogCheckSubobjects Fatal, LogAutomationController Verbose"
+
+echo ================================================================================================================================================
+echo Running tests with 100ms packet lag
+echo ================================================================================================================================================
+
+%UNREAL_EDITOR% %PROJECT_FILE% -Deterministic -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ini:Engine:[PacketSimulationSettings]:PktLag=100 -ini:EditorPerProjectUserSettings:[/Script/UnrealEd.LevelEditorPlaySettings]:PlayNetMode=PIE_ListenServer -ExecCmds="Automation RunTests Project.Functional Tests.Tests;quit" -StdOut -LogCmds="Global Warning, LogCheckSubobjects Fatal, LogAutomationController Verbose"
+
+echo ================================================================================================================================================
+echo Running tests with 5% packet loss and 100ms packet lag
+echo ================================================================================================================================================
+
+%UNREAL_EDITOR% %PROJECT_FILE% -Deterministic -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ini:Engine:[PacketSimulationSettings]:PktLag=100,PktLoss=5 -ini:EditorPerProjectUserSettings:[/Script/UnrealEd.LevelEditorPlaySettings]:PlayNetMode=PIE_ListenServer -ExecCmds="Automation RunTests Project.Functional Tests.Tests;quit" -StdOut -LogCmds="Global Warning, LogCheckSubobjects Fatal, LogAutomationController Verbose"
+
 
 endlocal
 @echo on
