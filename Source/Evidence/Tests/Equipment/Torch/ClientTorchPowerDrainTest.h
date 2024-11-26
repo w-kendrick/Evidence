6da0af03773121ac281038ac234cae5c41bcc339
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-11-26 21:19:30 +0000

    Created client torch power drain test

---
+++ Source/Evidence/Tests/Equipment/Torch/ClientTorchPowerDrainTest.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Evidence/Tests/MasterTest.h"
+#include "ClientTorchPowerDrainTest.generated.h"
+
+class ATorch;
+
+/**
+ * 
+ */
+UCLASS()
+class EVIDENCE_API AClientTorchPowerDrainTest : public AMasterTest
+{
+	GENERATED_BODY()
+
+public:
+	AClientTorchPowerDrainTest();
+
+	void StartTest() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* UseAction;
+
+private:
+	UPROPERTY()
+	ATorch* ServerTorch = nullptr;
+
+	UPROPERTY()
+	ATorch* Client1Torch = nullptr;
+
+	UPROPERTY()
+	ATorch* Client2Torch = nullptr;
+
+	float PowerAtDeactivation = 0.0f;
+
+	void OnEquip();
+	void OnActivate();
+	void OnDeactivate();
+	
+};
