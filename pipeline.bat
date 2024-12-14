1796fa89bbeaa2fd9357af707556aeb0db97f36d
William Kendrick <78029204+w-kendrick@users.noreply.github.com>
2024-12-14 20:50:04 +0000

    Updated project file path to use current directory

--- pipeline.bat
+++ pipeline.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set UNREAL_EDITOR="C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
-set PROJECT_FILE="C:\Users\willk\Documents\Unreal Projects\Evidence\Evidence.uproject"
+set PROJECT_FILE="%CD%\Evidence.uproject"
 
 %UNREAL_EDITOR% %PROJECT_FILE% -Deterministic -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ini:EditorPerProjectUserSettings:[/Script/UnrealEd.LevelEditorPlaySettings]:PlayNetMode=PIE_ListenServer -ExecCmds="Automation RunTests Project.Functional Tests.Tests;quit" -StdOut -ReportOutputPath="%CD%\Saved\Automation\Reports" -LogCmds="Global Warning, LogCheckSubobjects Fatal, LogAutomationController Verbose"
 
